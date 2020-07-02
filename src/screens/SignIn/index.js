ee982ed6fb0b2c746b7c4742bfd3c64069da00c5
rodrigoaives <rodrigoaives@outlook.com>
2020-07-02 15:46:39 -0300

    Sigin with api implemented

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { signIn } from './SignInActions';
 
 const SignIn = (props) => {
-  const { signIn } = props;
+  const { signIn, account } = props;
+
+  if (account) {
+    return <Redirect to='/manage/links' />;
+  }
 
   const submitHandler = (e) => {
     e.preventDefault();
-    signIn({ email: 'rodrigoaives2@outlook.com', password: '123456' });
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    signIn(data);
   };
-
+ 
   return (
     <div className='container h-100 pt-5'>
       <h1>Sign In</h1>
@@ -18,11 +24,11 @@ const SignIn = (props) => {
         <form onSubmit={submitHandler}>
           <div className='form-group'>
             <label>E-mail</label>
-            <input type='text' className='form-control' />
+            <input type='text' className='form-control' name="email" />
           </div>
           <div className='form-group'>
             <label>Password</label>
-            <input type='password' className='form-control' />
+            <input type='password' className='form-control' name="password" />
           </div>
           <div>
             <button className='btn btn-primary btn-round'>Submit</button>
