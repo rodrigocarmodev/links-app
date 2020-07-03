de472903ac789f28d1253945cc427adb9aa19c8f
rodrigoaives <rodrigoaives@outlook.com>
2020-07-03 17:56:49 -0300

    Helpers to get token expires

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { getToken } from './account';
+import { getTokenExpire } from './jwt';
+import { secondsToReadableTime } from './datetime';
 
 export const getApiUrl = (path) => {
   return `http://localhost:3001${path}`;
@@ -8,6 +10,12 @@ export const getApiUrl = (path) => {
 export const getHeaders = () => {
   const token = getToken();
   if (!token) return {};
+
+  const expires = getTokenExpire(token);
+  const secondsToExpire = expires - Date.now() / 1000;
+  const readableTime = secondsToReadableTime(secondsToExpire);
+
+  console.log('*** getHeaders.readableTime', readableTime);
 
   return {
     Authorization: `Bearer ${token}`,
